fcc8e323ac6e838401846243adf38c03d125cf3a
hmm-a11y <2951696462@qq.com>
2020-04-21 10:26:19 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/sdwu/android02/aclass/sn170507180128/ch10Activity1.java
@@ -0,0 +1,12 @@
+package cn.edu.sdwu.android02.aclass.sn170507180128;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ch10Activity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
